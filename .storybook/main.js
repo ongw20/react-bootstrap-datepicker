350e84d7d1d93b015f5e1831db1b7e8066877b38
ong <ongw20@outlook.com>
2020-07-15 18:43:53 +0800

    feat: add basic files

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+}
